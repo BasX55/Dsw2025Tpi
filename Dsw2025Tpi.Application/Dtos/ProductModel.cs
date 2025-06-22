06d800eda1caf180a48d5b8db938df931d81522d
Joaquinm1293 <Joaquin.Moran@alu.frt.utn.edu.ar>
2025-06-22 19:02:53 -0300

    se agrego products.json, 2 clases de excepciones y ProductModel

---
+++ Dsw2025Tpi.Application/Dtos/ProductModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Dsw2025Ej15.Application.Dtos;
+
+public record ProductModel
+{
+    public record Request(string Sku, string Name, string Description, decimal CurrentUnitPrice, int StockQuantity, bool IsActive);
+
+    public record Response(Guid Id);
+}
---
+++ Dsw2025Tpi.Application/Exceptions/AccessDeniedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Application.Exceptions
+{
+    public class AccessDeniedException : Exception
+    {
+        //excepcion para cuando el cliente intenta hacer operaciones de administrador
+        public AccessDeniedException(string message) : base(message)
+        {
+        }
+        public AccessDeniedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
