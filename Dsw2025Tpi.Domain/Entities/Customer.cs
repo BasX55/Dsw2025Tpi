43ef421a9a6c6e4ba445940900728378f64b1746
Joaquinm1293 <Joaquin.Moran@alu.frt.utn.edu.ar>
2025-06-21 22:02:06 -0300

    Se agregaron nuevas entidades en la capa Domain

---
+++ Dsw2025Tpi.Domain/Entities/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Domain.Entities
+{
+    public class Customer
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<Order> Orders { get; set; }
+
+
+    }
+}
---
+++ Dsw2025Tpi.Domain/Entities/OrderItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Domain.Entities
+{
+    public class OrderItem : EntityBase
+    {
+        public OrderItem() : base()
+        {
+
+        }
+
+        public Guid OrderId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        // Navigation properties
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+        
+        
+
+        /* SEGUNDA OPCION O ALTERNATIVA
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Subtotal => Quantity * UnitPrice;
+
+        public Product Product { get; set; }
+
+         */
+    }
+}
